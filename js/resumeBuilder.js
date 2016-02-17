4a1f08016722c891d11f392019444b524a81e9f9
Alan Spiewak <spiewak@infionline.net>
2016-02-17 13:58:39 -0500

    bug: Record dead-end coding
    A lot of work went into a programming dead end from misunderstanding
    class assignments. This commit records the work, before I rip it out
    to match where we're supposed to be per class instructions.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,76 @@
+
+var bio = {
+	name: "Alan R. Spiewak",
+	role: "Software Developer",
+	email: "spiewak@infionline.net",
+	mobile: "(336) 430-0262",
+	github: "arspiewak",
+	location: "Greensboro, NC (USA)",
+	bioPic: "https://avatars3.githubusercontent.com/u/15353005?v=3&amp;s=460",
+	welcomeMsg: "Greetings and peace to all.",
+	skills: ["HTML/CSS beginner", "JavaScript newbie", "Former data administrator"]
+};
+
+var work = {};
+work.position = "Student developer";
+work.employer = "Unemployed";
+work.yearsWorked = 1;
+work.city = "Greensboro, NC";
+
+var educ = {};
+educ["name"] = "University of North Carolina at Greensboro";
+educ["years"] = "2009-2014";
+educ["city"] = "Greensboro, NC";
+
+
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedRole);
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").prepend(formattedName);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.email);
+$("#header").append(formattedEmail);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
+$("#header").append(formattedMobile);
+
+var formattedGithub = HTMLgithub.replace("%data%", bio.github);
+$("#header").append(formattedGithub);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+$("#header").append(formattedLocation);
+
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$("#header").append(formattedBioPic);
+
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+$("#header").append(formattedWelcomeMsg);
+
+$("#header").append(HTMLskillsStart);
+var formattedSkill;
+var i;
+
+for (i = 0; i < bio.skills.length; i++) {
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+    $("#header").append(formattedSkill);
+}
+
+// Construct the workExperience section
+$("#workExperience").append(HTMLworkStart);
+
+var formattedWorkTitle = HTMLworkTitle.replace("%data%", work["position"]);
+$("#workExperience").append(formattedWorkTitle);
+
+// Construct the #education section
+$("#education").append(HTMLschoolStart);
+
+var formattedSchoolName = HTMLschoolName.replace("%data%", educ.name);
+$("#education").append(formattedSchoolName);
+
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formatted[] = HTML[].replace("%data%", bio.[]);
+$("#header").append(formatted[]);
+
+*/
