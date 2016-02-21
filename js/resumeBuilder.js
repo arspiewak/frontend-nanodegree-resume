71cd8408e319a0b54659072f1dd1ab1fba872ded
Alan Spiewak <spiewak@infionline.net>
2016-02-21 01:36:11 -0500

    feat: Add display of projects to the page
    Some minor adjustments were made in style.css to make things look
    better, and the "internationalize" button was moved to the header
    section so its action can be seen in real time.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,9 @@
-
+/* resumeBuilder.js 	Alan Spiewak 	Part of Front-End Project 2, Interactive Resume
+ *
+ * Code sections appear in this file in the order they were assigned within the "Basic
+ * Javascript" course (unless dependencies require otherwise).
+ *
+ */
 var bio = {
 	name: "Alan R. Spiewak",
 	role: "Software Developer",
@@ -178,21 +183,79 @@ if (bio.skills.length > 0) {
 
 	for (i = 0; i < bio.skills.length; i++) {
 	    formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-    	$("#topContacts").append(formattedSkill);
+		$("#topContacts").append(formattedSkill);
 	}
 }
 
-// Truth in advertising for the "Work Experience" section header
+/*
+ * Truth in advertising for the "Work Experience" section header. My real resume's
+ * a lot longer than this.
+ */
 $("h2:first").append(" (Selected)");
 
-// Populate the work section. Replaced for-in loop with for() per style sheet
-
-for (i = 0; i < work.jobs.length; i++) {
-	$("#workExperience").append(HTMLworkStart);
-	str = HTMLworkEmployer.replace("%data%",work.jobs[i].employer) +
-		HTMLworkTitle.replace("%data%",work.jobs[i].title) +
-		HTMLworkDates.replace("%data%",work.jobs[i].dates) +
-		HTMLworkLocation.replace("%data%",work.jobs[i].location) +
-		HTMLworkDescription.replace("%data%",work.jobs[i].description);
-	$(".work-entry:last").append(str);
+/*
+ * Populate the work section. Replaced for-in loop with for() per style sheet. Here
+ * and elsewhere, we build a single string for each unit because of append()'s
+ * automagic management of end tags. If two strings are meant to share tag bookends,
+ * append() "helps" us by supplying an end tag after the first one. Rather than second-
+ * guessing tag locations inside the format strings, we just lump 'em all in together.
+ */
+function displayWork() {
+	for (i = 0; i < work.jobs.length; i++) {
+		$("#workExperience").append(HTMLworkStart);
+		str = HTMLworkEmployer.replace("%data%",work.jobs[i].employer) +
+			HTMLworkTitle.replace("%data%",work.jobs[i].title) +
+			HTMLworkDates.replace("%data%",work.jobs[i].dates) +
+			HTMLworkLocation.replace("%data%",work.jobs[i].location) +
+			HTMLworkDescription.replace("%data%",work.jobs[i].description);
+		$(".work-entry:last").append(str);
+	}
 }
+displayWork();
+
+/*
+ * Function used by #button's click event to modify the value of #name
+ * Note differences from class solution: (1) No argument passed from helper.js.
+ * (2) The last nameParts element is capitalized, not the second. Some of us use
+ * more than two names. This doesn't handle 'Jr.' etc. (3) Join() is used to
+ * reconstruct the name.
+ */
+function inName() {
+	var str = $("#name")[0].innerText;
+	var nameParts = str.split(" ");
+	var lastPart = nameParts.length - 1;
+	// Initial cap for first name
+	nameParts[0] = str.slice(0,1).toUpperCase() +
+		nameParts[0].slice(1).toLowerCase();
+	// All caps for last name
+	nameParts[lastPart] = nameParts[lastPart].toUpperCase();
+	return nameParts.join(" ");
+}
+
+// Add a button at the bottom of the header to exercise a capitalization assignment
+$("#header").append(internationalizeButton);
+
+/* Display of the projects section is managed through properties of the project object,
+ * including the functions that follow.
+ */
+project.display = function() {
+	var projLen = this.projects.length;
+	var addDisplay = function(s) { $("#projects").append(s); }
+	var thisProj = {};
+
+	for (i = 0; i < projLen; i++) {
+		addDisplay(HTMLprojectStart);
+		thisProj = this.projects[i];
+		$(".project-entry:last").append (
+			HTMLprojectTitle.replace("%data%", thisProj.title) +
+			HTMLprojectDates.replace("%data%", thisProj.dates) +
+			HTMLprojectDescription.replace("%data%", thisProj.description) +
+			HTMLprojectImage.replace("%data%", thisProj.image)
+		);
+	}
+
+	return true;
+}
+
+// Don't forget to call the function!
+project.display();
